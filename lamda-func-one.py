d8944822815241173b7c7468970b657e8249b8dd
chakravartikothadiya <chakravartikothadiya@gmail.com>
2023-08-18 12:46:46 -0400

    4th

--- lamda-func-one.py
+++ lamda-func-one.py
@@ -2,5 +2,5 @@ import requests
 
 def lambda_handler(event, context):
     response = requests.get("https://www.example.com")
-    return {"statusCode": 200, "body": "Hello, world! Updateddddddd"}
+    return {"statusCode": 200, "body": "Hello, world! This is through Github Actionss"}
 
