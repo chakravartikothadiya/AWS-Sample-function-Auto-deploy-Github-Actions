b0c36539ba41c9f25f295bf47967e606188f2707
chakravartikothadiya <chakravartikothadiya@gmail.com>
2023-08-19 19:58:31 -0400

    8th

--- lamda-func-one.py
+++ lamda-func-one.py
@@ -1,6 +1,14 @@
 import requests
+import os
 
 def lambda_handler(event, context):
+
+    var1_value = os.environ.get('Var1')
+    if var1_value:
+        print(f"Value of Var1: {var1_value}")
+    else:
+        print("Var1 environment variable not set.")
+
     response = requests.get("https://www.example.com")
     return {"statusCode": 200, "body": "Hello, world! This is through Github Actionss"}
 
